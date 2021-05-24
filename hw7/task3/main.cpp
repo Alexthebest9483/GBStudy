9d2244bd132414a70c28164cce770d9afadc77ca
SkillF00L <61927882+SkillF00L@users.noreply.github.com>
2021-05-25 02:58:55 +0300

    Update main.cpp

--- hw7/task3/main.cpp
+++ hw7/task3/main.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <locale.h>
-#include "mylib.h"
 
 using namespace std;
 
 #define ARR_SIZE 10
 
-#define SwapINT(T, a, b) \
+#define SwapINT(a, b) \
     { \
-        T = a; \
+        auto tmp = a; \
         a = b; \
-        b = T; \
+        b = tmp; \
     }
 
 void sortArr(int Arr[])
 {
-    int temp;
     for (size_t i = 0; i < ARR_SIZE; ++i)
         for (int j = 0; j < ARR_SIZE - i - 1; ++j)
             if (Arr[j] > Arr[j + 1])
-                SwapINT(temp, Arr[j], Arr[j + 1]);
+                SwapINT(Arr[j], Arr[j + 1]);
 }
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "Russian");
 
     int Arr[ARR_SIZE];
     for (size_t i = 0; i < ARR_SIZE; ++i)
@@ -35,5 +33,5 @@ int main()
     for (size_t i = 0; i < ARR_SIZE; ++i)
         cout << Arr[i] << " ";
 
-	return 0;
+    return 0;
 }
